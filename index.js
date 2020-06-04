6343a023ee96aafe56356e470ccf3c595d8a1d5d
Pranjalpatel1 <pranjalpatel2244@gmail.com>
2020-06-04 23:10:00 +0530

    basic version without ui

---
+++ index.js
@@ -0,0 +1,14 @@
+ const express = require('express');
+ const app = express();
+ const connectDB = require('./config/db');
+
+ connectDB();
+ app.use(express.json({extended:false}));
+
+//routes
+app.use('/',require('./routes/index'));
+app.use('/api/url',require('./routes/url'));
+
+const port=3000;
+app.listen(port,()=> console.log(`server running on port $(port)`));
+ 
