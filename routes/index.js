562b94fdfc287fc5e6e4260746663a3b677b7ba2
Pranjalpatel1 <pranjalpatel2244@gmail.com>
2020-06-05 21:26:42 +0530

    final v-2

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,18 @@ const router= express.Router();
 
 const Url = require("../model/url");
 router.get('', async (req,res)=> {
-try{
-    const url = await Url.findOne({urlCode: req.params.code});
-    if(url)
-    {
-        return res.redirect(url.longUrl);
-    }
-    else{
-        return res.status(404).json("no url found");
-    }
-}
-catch(err)
-{
-    console.error(err);
-    res.status(500).json('server error');
-}
-});
-
-module.exports=router;
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+    
+        if (url) {
+          return res.redirect(url.longUrl);
+        } else {
+          return res.status(404).json('No url found');
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
+      }
+    });
+    
+    module.exports = router;
