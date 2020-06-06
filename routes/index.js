994540bff7965cebc08d345841591518abf1cc2e
Pranjalpatel1 <pranjalpatel2244@gmail.com>
2020-06-06 12:37:40 +0530

    version with ui

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/:code', async (req,res)=> {
         console.log(req.params.code);
         const url = await Url.findOne({ urlCode: req.params.code });
         if (url) {
+          url.clicks++;
+          url.save();
           return res.redirect(url.longUrl);
         } else {
           return res.status(404).json('No url found');
